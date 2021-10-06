7f68dd906579b3cd255fa3098f9b1d6d44e412b3
OuYangMinOa <wesley91345@gmail.com>
2021-10-06 17:40:48 +0800

    v1.0.0

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from distutils.core import setup
 setup(
-  name = 'ou_Axion_limit',         # How you named your package folder (MyLib)
-  packages = ['ou_Axion_limit'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Calculate the AXion G_gamma limit',   # Give a short description about your library
-  long_description = open("CHANGELOG.txt").read(),
-  author = 'OUYANG MINWEI',                   # Type in your name
-  author_email = 'wesley91345@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/OuYangMinOa/ou_Axion_limit',   # Provide either the link to your github or to your website
-  download_url = '',    # I explain this later on
-  keywords = ['Axion', 'TASEH', 'drak matter'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name         = 'ou_Axion_limit',         # your package folder (ou_Axion_limit)
+  packages     = ['ou_Axion_limit'],   # Chose the same as "name"
+  version      = '1.0.0',      # your version
+  license      = 'MIT',        # https://help.github.com/articles/licensing-a-repository
+  description  = 'Calculate the AXion G_gamma limit',   # Give a short description about your library
+  long_description = open("CHANGELOG.txt").read(),     # Give a long description about your library
+  author       = 'OUYANG MINWEI',                    # your name
+  author_email = 'wesley91345@gmail.com',      # your E-Mail
+  url          = 'https://github.com/OuYangMinOa/ou_Axion_limit',   #  your github or to your website
+  download_url = '',
+  keywords     = ['Axion', 'TASEH', 'drak matter'],   # Keywords that define your package best
+  install_requires = [            # required package
           'matplotlib',
           'numpy',
       ],
-  classifiers=[
+  classifiers = [
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',       # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
